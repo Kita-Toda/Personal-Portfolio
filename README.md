f366c248be160139502e8fbb8ef721e5346fe811
Xtian03 <calba03@gmail.com>
2017-08-03 11:39:47 +1000

    Updated HTML/CSS & added jQuery

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 
 # Personal Portfolio
 
-  I have a principle of always thinking outside the ...
+  I live by a principle of always thinking outside the ...
 
     - Text of <nav> should be vertically aligned .vertical text
     - animate CSS Transform: hover either left or right
 
 # Languages
   - HTML | CSS
-  -
+  - jQuery
 
 # Frameworks
   - Foundation
 
 # Fonts
   - Google Fonts
+  - Awesome Fonts
---
+++ js/main.js
@@ -0,0 +1,103 @@
+// BLOG
+
+$(document).ready(function() {
+  // console.log("jQuery working"); TESTING
+  $(".content .blog.box").hover(function() {
+    // console.log("Hovered Success"); TESTING
+    $(".text-content .blog.box h2").stop();
+    $(".text-content .blog.box h2").animate(
+      {
+        top: "-60px"
+      },
+      1000
+    );
+  });
+  $(".content .blog.box").mouseleave(function() {
+    // console.log("Hovered Success"); TESTING
+    $(".text-content .blog.box h2").stop();
+    $(".text-content .blog.box h2").animate(
+      {
+        top: "0px"
+      },
+      1000
+    );
+  });
+});
+
+// ABOUT
+
+$(document).ready(function() {
+  // console.log("jQuery working"); TESTING
+  $(".content .about.box").hover(function() {
+    // console.log("Hovered Success"); TESTING
+    $(".text-content .about.box h2").stop();
+    $(".text-content .about.box h2").animate(
+      {
+        right: "-90px"
+      },
+      1000
+    );
+  });
+  $(".content .about.box").mouseleave(function() {
+    // console.log("Hovered Success"); TESTING
+    $(".text-content .about.box h2").stop();
+    $(".text-content .about.box h2").animate(
+      {
+        right: "-22px"
+      },
+      1000
+    );
+  });
+});
+
+// CONTACT
+
+$(document).ready(function() {
+  // console.log("jQuery working"); TESTING
+  $(".content .contact.box").hover(function() {
+    // console.log("Hovered Success"); TESTING
+    $(".text-content .contact.box h2").stop();
+    $(".text-content .contact.box h2").animate(
+      {
+        left: "-110px"
+      },
+      1000
+    );
+  });
+  $(".content .contact.box").mouseleave(function() {
+    // console.log("Hovered Success"); TESTING
+    $(".text-content .contact.box h2").stop();
+    $(".text-content .contact.box h2").animate(
+      {
+        left: "-22px"
+      },
+      1000
+    );
+  });
+});
+
+// PORTFOLIO
+
+$(document).ready(function() {
+  // console.log("jQuery working"); TESTING
+  $(".content .portfolio.box").hover(function() {
+    // console.log("Hovered Success"); TESTING
+    $(".text-content .portfolio.box h2").stop();
+    $(".text-content .portfolio.box h2").animate(
+      {
+        bottom: "-50px"
+      },
+      1000
+    );
+  });
+  $(".content .portfolio.box").mouseleave(function() {
+    // console.log("Hovered Success"); TESTING
+    $(".text-content .portfolio.box h2").stop();
+    $(".text-content .portfolio.box h2").animate(
+      {
+        bottom: "0px"
+      },
+      1000
+    );
+  });
+});
